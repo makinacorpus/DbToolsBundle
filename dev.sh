25dab56ef11fdd330cab9eb46dccd4b5cd330a2a
Simon Mellerin <simon.mellerin@makina-corpus.com>
2023-11-23 17:22:48 +0100

    no-issue - update dev.sh

--- dev.sh
+++ dev.sh
@@ -37,11 +37,7 @@ do_checks() {
     docker compose -p db_tools_bundle_test exec phpunit composer checks
 }
 
-# Run PHPunit tests for all database vendors
-do_test() {
-    section_title "Composer install dependencies"
-    docker compose -p db_tools_bundle_test exec phpunit composer install
-
+do_test_mysql57() {
     section_title "Running tests with MySQL 5.7"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_mysql \
@@ -54,7 +50,9 @@ do_test() {
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mysql57:3306/test_db?serverVersion=5.7 \
         phpunit vendor/bin/phpunit $@
+}
 
+do_test_mysql80() {
     section_title "Running tests with MySQL 8"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_mysql \
@@ -67,7 +65,9 @@ do_test() {
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mysql80:3306/test_db?serverVersion=8 \
         phpunit vendor/bin/phpunit $@
+}
 
+do_test_mariadb11() {
     section_title "Running tests with MariaDB 11"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_mysql \
@@ -80,7 +80,15 @@ do_test() {
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mariadb11:3306/test_db?serverVersion=11.1.3-MariaDB \
         phpunit vendor/bin/phpunit $@
+}
 
+do_test_mysql() {
+    do_test_mysql57
+    do_test_mysql80
+    do_test_mariadb11
+}
+
+do_test_postgresql10() {
     section_title "Running tests with PostgreSQL 10"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_pgsql \
@@ -93,7 +101,9 @@ do_test() {
         -e DBAL_USER=postgres \
         -e DATABASE_URL="postgresql://postgres:password@postgresql10:5432/test_db?serverVersion=10&charset=utf8" \
         phpunit vendor/bin/phpunit $@
+}
 
+do_test_postgresql16() {
     section_title "Running tests with PostgreSQL 16"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_pgsql \
@@ -106,7 +116,14 @@ do_test() {
         -e DBAL_USER=postgres \
         -e DATABASE_URL="postgresql://postgres:password@postgresql16:5432/test_db?serverVersion=16&charset=utf8" \
         phpunit vendor/bin/phpunit $@
+}
 
+do_test_postgresql() {
+    do_test_postgresql10
+    do_test_postgresql16
+}
+
+do_test_sqlsrv2019() {
     section_title "Running tests with SQL Server 2019"
     docker compose -p db_tools_bundle_test exec \
         -e DBAL_DRIVER=pdo_sqlsrv \
@@ -119,6 +136,54 @@ do_test() {
         -e DBAL_USER=sa \
         -e DATABASE_URL="pdo-sqlsrv://sa:P%40ssword123@sqlsrv2019:1433/test_db?serverVersion=2019&charset=utf8&driverOptions[TrustServerCertificate]=true" \
         phpunit vendor/bin/phpunit $@
+}
+
+do_test_sqlsrv() {
+    do_test_sqlsrv2019
+}
+
+# Run PHPunit tests for all database vendors
+do_test_all() {
+    section_title "Composer install dependencies"
+    docker compose -p db_tools_bundle_test exec phpunit composer install
+
+    do_test_mysql57
+    do_test_mysql80
+    do_test_mariadb11
+    do_test_postgresql10
+    do_test_postgresql16
+    do_test_sqlsrv2019
+}
+
+do_test_notice() {
+    section_title "Test a specicif database vendor or version"
+    printf "\nThis action will allow you to test a specific vendor or version."
+    printf "\n"
+    printf "\nLaunch this action with one of these available options:"
+    printf "\n"
+    printf "\n  - ${GREEN}mysql${NC}: Launch test for MySQL 5.7, MySQL 8.0 & MariaDB 11"
+    printf "\n  - ${GREEN}mysql57${NC}: Launch test for MySQL 5.7"
+    printf "\n  - ${GREEN}mysql80${NC}: Launch test for MySQL 8.0"
+    printf "\n  - ${GREEN}mariadb11${NC}: Launch test for MariaDB 11"
+    printf "\n  - ${GREEN}postgresql${NC}: Launch test for PostgreSQL 10 & 16"
+    printf "\n  - ${GREEN}postgresql10${NC}: Launch test for PostgreSQL 10"
+    printf "\n  - ${GREEN}postgresql16${NC}: Launch test for PostgreSQL 16"
+    printf "\n  - ${GREEN}sqlsrv${NC}: Launch test for SQL Server 2019"
+    printf "\n  - ${GREEN}sqlsrv2019${NC}: Launch test for SQL Server 2019"
+    printf "\n\nYou can then use PHPUnit option as usual:"
+    printf "\n${GREEN}./dev.sh test mysql --filter AnonymizatorFactoryTest${NC}"
+    printf "\n\n"
+}
+
+do_test() {
+    suit=${1-}
+
+    if [[ -n $@ ]];then shift;fi
+
+    case $suit in
+        mysql57|mysql80|mariadb11|mysql|postgresql10|postgresql16|postgresql|sqlsrv2019|sqlsrv) do_test_$action "$@";;
+        *) do_test_notice;;
+    esac
 }
 
 # Display help
@@ -142,7 +207,9 @@ do_notice() {
     printf "\n  - ${GREEN}up${NC}: Start docker containers"
     printf "\n  - ${GREEN}down${NC}: Stop docker containers"
     printf "\n  - ${GREEN}checks${NC}: Launch composer checks (for Static analysis & Code style fixer)"
-    printf "\n  - ${GREEN}test${NC}: Run PHPunit tests for all database vendors"
+    printf "\n  - ${GREEN}test_all${NC}: Run PHPunit tests for all database vendors."
+    printf "\n              PHPUnit options can be used as usual: ${GREEN}./dev.sh test_all --filter AnonymizatorFactoryTest${NC}"
+    printf "\n  - ${GREEN}test${NC}: Run PHPunit tests for a specific database vendors or version"
     printf "\n  - ${GREEN}notice${NC}: Display this help"
     printf "\n\n"
 }
@@ -153,6 +220,6 @@ action=${1-}
 if [[ -n $@ ]];then shift;fi
 
 case $action in
-    build|up|down|checks|test|notice) do_$action "$@";;
+    build|up|down|checks|test_all|test|notice) do_$action "$@";;
     *) do_notice;;
 esac
