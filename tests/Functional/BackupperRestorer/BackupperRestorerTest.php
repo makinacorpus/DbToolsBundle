5823c702c2b43e4facd9868924f4b552dc9b3deb
Lonny Loquesol <lonny.loquesol@makina-corpus.com>
2024-02-19 17:35:07 +0100

    Issue #79 - Add backupper and restorer functional tests which use extra options

--- tests/Functional/BackupperRestorer/BackupperRestorerTest.php
+++ tests/Functional/BackupperRestorer/BackupperRestorerTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace MakinaCorpus\DbToolsBundle\Tests\Functional\BackupperRestorer;
 
+use Doctrine\DBAL\Platforms\MariaDBPlatform;
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\Persistence\ManagerRegistry;
 use MakinaCorpus\DbToolsBundle\Backupper\BackupperFactory;
 use MakinaCorpus\DbToolsBundle\Error\NotImplementedException;
@@ -11,9 +15,9 @@ use MakinaCorpus\DbToolsBundle\Restorer\RestorerFactory;
 use MakinaCorpus\DbToolsBundle\Test\FunctionalTestCase;
 
 /**
- * This class will successivly test Backupper and Restorer.
+ * This class will successively test Backupper and Restorer.
  *
- * 1. Create a table and but data in it
+ * 1. Create a table and put data in it
  * 2. Perform Backup
  * 3. Modify data and structure in the previously created table
  * 4. Restore the dump from step 2
@@ -109,6 +113,57 @@ class BackupperRestorerTest extends FunctionalTestCase
     }
 
     /**
+     * @depends testRestorer
+     */
+    public function testBackupperWithExtraOptions(): void
+    {
+        $connection = $this->getConnection();
+        $mockDoctrineRegistry = $this->createMock(ManagerRegistry::class);
+        $mockDoctrineRegistry
+            ->method('getConnection')
+            ->willReturn($connection)
+        ;
+
+        $backupperFactoy = new BackupperFactory($mockDoctrineRegistry, [
+            'mariadb' => 'mariadb-dump',
+            'mysql' => 'mysqldump',
+            'postgresql' => 'pg_dump',
+            'sqlite' => 'sqlite3',
+        ]);
+
+        try {
+            $backupper = $backupperFactoy->create('');
+        } catch (NotImplementedException $e) {
+            $this->markTestSkipped('Driver unsupported: ' . \getenv('DBAL_DRIVER'));
+        }
+
+        $backupper->checkBinary();
+        $backupFilename = $this->prepareAndGetBackupFilename($backupper->getExtension());
+        $platform = $connection->getDatabasePlatform();
+
+        $backupper
+            ->setDestination($backupFilename)
+            ->setExtraOptions(match (true) {
+                $platform instanceof MariaDBPlatform => '-v --no-tablespaces --add-drop-table --skip-quote-names',
+                $platform instanceof MySQLPlatform => '-v --no-tablespaces --add-drop-table --skip-quote-names',
+                $platform instanceof PostgreSQLPlatform => '-v --no-owner -Z 5 --lock-wait-timeout=120',
+                $platform instanceof SqlitePlatform => '-bail -readonly', // No interesting options for SQLite.
+                default => $this->markTestSkipped('Driver unsupported: ' . \getenv('DBAL_DRIVER')),
+            })
+            ->setVerbose(false) // Enable via an extra option.
+            ->startBackup()
+        ;
+
+        foreach ($backupper as $data) {
+            // Nothing to do there.
+        }
+
+        $backupper->checkSuccessful();
+
+        self::assertFileExists($backupFilename);
+    }
+
+    /**
      * @depends testBackupper
      */
     public function testRestorer(): void
@@ -150,6 +205,78 @@ class BackupperRestorerTest extends FunctionalTestCase
         $restorer
             ->setBackupFilename($this->prepareAndGetBackupFilename($restorer->getExtension()))
             ->setVerbose(true)
+            ->startRestore()
+        ;
+
+        foreach ($restorer as $data) {
+            // Nothing to do there.
+        }
+        $restorer->checkSuccessful();
+
+        // Now we check data integrity:
+        // - All data from initial insert (see self::createTestData) should be there
+        // - Deleted data from our previous modifications should not be
+
+        $schemaManager = $connection->createSchemaManager();
+        self::assertTrue($schemaManager->tablesExist('table_in_backup_1'));
+        self::assertTrue($schemaManager->tablesExist('table_in_backup_2'));
+
+
+        $this->assertSame(
+            6,
+            $this->getConnection()->executeQuery('select count(*) from table_in_backup_1')->fetchOne(),
+        );
+    }
+
+    /**
+     * @depends testBackupperWithExtraOptions
+     */
+    public function testRestorerWithExtraOptions(): void
+    {
+        $connection = $this->getConnection();
+        $mockDoctrineRegistry = $this->createMock(ManagerRegistry::class);
+        $mockDoctrineRegistry
+            ->method('getConnection')
+            ->willReturn($connection)
+        ;
+
+        // First we do some modifications to the database
+        $this->dropTableIfExist('table_in_backup_2');
+
+        $connection
+            ->createQueryBuilder()
+            ->delete('table_in_backup_1')
+            ->where('id = 1')
+            ->executeStatement()
+        ;
+
+        $restorerFactoy = new RestorerFactory($mockDoctrineRegistry, [
+            'mariadb' => 'mariadb',
+            'mysql' => 'mysql',
+            'postgresql' => 'pg_restore',
+            'sqlite' => 'sqlite3',
+        ]);
+
+        try {
+            $restorer = $restorerFactoy->create('');
+        } catch (NotImplementedException $e) {
+            $this->markTestSkipped('Driver unsupported: ' . \getenv('DBAL_DRIVER'));
+        }
+
+        $restorer->checkBinary();
+        $platform = $connection->getDatabasePlatform();
+        $connection->close();
+
+        $restorer
+            ->setBackupFilename($this->prepareAndGetBackupFilename($restorer->getExtension()))
+            ->setExtraOptions(match (true) {
+                $platform instanceof MariaDBPlatform => '-v --no-auto-rehash --skip-progress-reports',
+                $platform instanceof MySQLPlatform => '-v --no-auto-rehash',
+                $platform instanceof PostgreSQLPlatform => '-v -j 2 --disable-triggers --clean --if-exists',
+                $platform instanceof SqlitePlatform => '-bail', // No interesting options for SQLite.
+                default => $this->markTestSkipped('Driver unsupported: ' . \getenv('DBAL_DRIVER')),
+            })
+            ->setVerbose(false) // Enable via an extra option.
             ->startRestore()
         ;
 
