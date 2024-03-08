06400fe960f608551dfdd55a0c5ae0cf57fb5ec9
Simon Mellerin <simon.mellerin@makina-corpus.com>
2024-03-08 16:55:32 +0100

    #86 - FloatAnonymizer - add possibility to anonymize value by adding noise

--- docs/content/anonymization/core-anonymizers.md
+++ docs/content/anonymization/core-anonymizers.md
@@ -231,7 +231,11 @@ customer:
 
 ## FloatAnonymizer
 
-This *Anonymizer* will fill configured column with a random float between two values and a given precision (default 2).
+Anonymize float by:
+- randomly choosing an integer in a range delimited by `min` and `max` options
+- altering the initial value by adding it a random value picked in a range computed from the `delta` or `percent` options
+
+You may also specify a `precision` (default 2).
 
 ::: code-group
 ```php [Attribute]
@@ -247,6 +251,24 @@ class Customer
     // ...
 
     #[ORM\Column]
+    // Will fill the size column with a random float // [!code ++]
+    // in the [min, max] interval. // [!code ++]
+    #[Anonymize(type: 'float', options: ['min' => 10, 'max' => 99, 'precision' => 4])] // [!code ++]
+    private ?float $size = null;
+
+    #[ORM\Column]
+    // Will add to each size value a random integer // [!code ++]
+    // in the [-delta, +delta] interval. // [!code ++]
+    // In this example, an integer between -15.5 and 15.5 will be // [!code ++]
+    // added to the initial value. // [!code ++]
+    #[Anonymize(type: 'float', options: ['delta' => 15.5, 'precision' => 4])] // [!code ++]
+    private ?float $size = null;
+
+    #[ORM\Column]
+    // Will add to each size value a random percent // [!code ++]
+    // of the initial value in the [-percent%, +percent%] interval. // [!code ++]
+    // In this example, a value between -10% and 10% of the initial value // [!code ++]
+    // will be added to the initial value. // [!code ++]
     #[Anonymize(type: 'float', options: ['min' => 10, 'max' => 99, 'precision' => 4])] // [!code ++]
     private ?float $size = null;
 
@@ -258,9 +280,29 @@ class Customer
 # config/anonymization.yaml
 
 customer:
+    # Will fill the size column with a random float
+    # in the [min, max] interval.
     size:
         anonymizer: float
         options: {min: 120, max: 300, precision: 4}
+
+customer:
+    # Will add to each size value a random integer
+    # in the [-delta, +delta] interval.
+    # In this example, an integer between -15.5 and 15.5 will be
+    # added to the initial value.
+    size:
+        anonymizer: float
+        options: {delta: 15.5}
+
+customer:
+    # Will add to each size value a random percent
+    # of the initial value in the [-percent%, +percent%] interval.
+    # In this example, a value between -10% and 10% of the initial value
+    # will be added to the initial value.
+    size:
+        anonymizer: float
+        options: {percent: 10}
 #...
 ```
 :::
--- tests/Functional/Anonymizer/Core/FloatAnonymizerTest.php
+++ tests/Functional/Anonymizer/Core/FloatAnonymizerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MakinaCorpus\DbToolsBundle\Tests\Functional\Anonymizer\Core;
 
+use Doctrine\DBAL\Platforms\SQLServerPlatform;
 use MakinaCorpus\DbToolsBundle\Anonymization\Anonymizator;
 use MakinaCorpus\DbToolsBundle\Anonymization\Anonymizer\AnonymizerRegistry;
 use MakinaCorpus\DbToolsBundle\Anonymization\Anonymizer\Options;
@@ -25,15 +26,15 @@ class FloatAnonymizerTest extends FunctionalTestCase
             [
                 [
                     'id' => '1',
-                    'data' => '1.5',
+                    'data' => '10.5',
                 ],
                 [
                     'id' => '2',
-                    'data' => '2.5',
+                    'data' => '20.5',
                 ],
                 [
                     'id' => '3',
-                    'data' => '3.5',
+                    'data' => '30.5',
                 ],
                 [
                     'id' => '4',
@@ -42,7 +43,7 @@ class FloatAnonymizerTest extends FunctionalTestCase
         );
     }
 
-    public function testAnonymize(): void
+    public function testAnonymizeWithMinAndMax(): void
     {
         $config = new AnonymizationConfig();
         $config->add(new AnonymizerConfig(
@@ -53,41 +54,168 @@ class FloatAnonymizerTest extends FunctionalTestCase
         ));
 
         $anonymizator = new Anonymizator(
-            $this->getConnection(),
+            $connection = $this->getConnection(),
             new AnonymizerRegistry(),
             $config
         );
 
         $this->assertSame(
-            1.5,
-            (float) $this->getConnection()->executeQuery('select data from table_test where id = 1')->fetchOne(),
+            10.5,
+            (float) $connection->executeQuery('select data from table_test where id = 1')->fetchOne(),
         );
 
         foreach ($anonymizator->anonymize() as $message) {
         }
 
-        $datas = $this->getConnection()->executeQuery('select data from table_test order by id asc')->fetchFirstColumn();
+        $datas = $connection->executeQuery('select data from table_test order by id asc')->fetchFirstColumn();
 
         $data = (float) $datas[0];
         $this->assertNotNull($data);
-        $this->assertNotSame(1.5, $data);
-        $this->assertTrue($data >= 2 && $data <= 5.5);
-        $this->assertSame(\round($data, 6), \round($data, 6));
+        $this->assertNotSame(10.5, $data);
+        $this->assertGreaterThanOrEqual(2, $data);
+        $this->assertLessThanOrEqual(5.5, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 6));
+        }
 
         $data = (float) $datas[1];
         $this->assertNotNull($data);
-        $this->assertNotSame(2.5, $data);
-        $this->assertTrue($data >= 2 && $data <= 5.5);
-        $this->assertSame(\round($data, 6), \round($data, 6));
+        $this->assertNotSame(20.5, $data);
+        $this->assertGreaterThanOrEqual(2, $data);
+        $this->assertLessThanOrEqual(5.5, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 6));
+        }
 
         $data = (float) $datas[2];
         $this->assertNotNull($data);
-        $this->assertNotSame(3.5, $data);
-        $this->assertTrue($data >= 2 && $data <= 5.5);
-        $this->assertSame(\round($data, 6), \round($data, 6));
+        $this->assertNotSame(30.5, $data);
+        $this->assertGreaterThanOrEqual(2, $data);
+        $this->assertLessThanOrEqual(5.5, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 6));
+        }
 
         $this->assertNull($datas[3]);
 
         $this->assertCount(4, \array_unique($datas), 'All generated values are different.');
+    }
+
+    public function testAnonymizeWithDelta(): void
+    {
+        $config = new AnonymizationConfig();
+        $config->add(new AnonymizerConfig(
+            'table_test',
+            'data',
+            'float',
+            new Options(['delta' => 5.2, 'precision' => 4])
+        ));
+
+        $anonymizator = new Anonymizator(
+            $connection = $this->getConnection(),
+            new AnonymizerRegistry(),
+            $config
+        );
+
+        $this->assertSame(
+            10.5,
+            (float) $connection->executeQuery('select data from table_test where id = 1')->fetchOne(),
+        );
+
+        foreach ($anonymizator->anonymize() as $message) {
+        }
+
+        $datas = $connection->executeQuery('select data from table_test order by id asc')->fetchFirstColumn();
+
+        $data = (float) $datas[0];
+        $this->assertNotNull($data);
+        // Initial value is 10.5, we added a value in [-5.2, 5.2]
+        // so current value should be in [10.5-5.2, 10.5+5.2]
+        $this->assertGreaterThanOrEqual(5.3, $data);
+        $this->assertLessThanOrEqual(15.7, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 4));
+        }
+
+        $data = (float) $datas[1];
+        $this->assertNotNull($data);
+        // Initial value is 20.5, we added a value in [-5.2, 5.2]
+        // so current value should be in [20.5-5.2, 20.5+5.2]
+        $this->assertGreaterThanOrEqual(15.3, $data);
+        $this->assertLessThanOrEqual(25.7, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 4));
+        }
+
+        $data = (float) $datas[2];
+        $this->assertNotNull($data);
+        // Initial value is 30.5, we added a value in [-5.2, 5.2]
+        // so current value should be in [30.5-5.2, 30.5+5.2]
+        $this->assertGreaterThanOrEqual(25.3, $data);
+        $this->assertLessThanOrEqual(35.7, $data);
+        // Precision does not work well on SQL server.
+        if (!$connection->getDatabasePlatform() instanceof SQLServerPlatform) {
+            $this->assertSame($data, \round($data, 4));
+        }
+
+        $this->assertNull($datas[3]);
+    }
+
+    public function testAnonymizeWithPercent(): void
+    {
+        $config = new AnonymizationConfig();
+        $config->add(new AnonymizerConfig(
+            'table_test',
+            'data',
+            'float',
+            new Options(['percent' => 5])
+        ));
+
+        $anonymizator = new Anonymizator(
+            $connection = $this->getConnection(),
+            new AnonymizerRegistry(),
+            $config
+        );
+
+        $this->assertSame(
+            10.5,
+            (float) $connection->executeQuery('select data from table_test where id = 1')->fetchOne(),
+        );
+
+        foreach ($anonymizator->anonymize() as $message) {
+        }
+
+        $datas = $connection->executeQuery('select data from table_test order by id asc')->fetchFirstColumn();
+
+        $data = (float) $datas[0];
+        $this->assertNotNull($data);
+        // Initial value is 10.5, we added a random percent of
+        // this value in [-5%, 5%],
+        // so current value should be in [10.5*0.95, 10.5*1.05]
+        $this->assertGreaterThanOrEqual(9.975, $data);
+        $this->assertLessThanOrEqual(11.025, $data);
+
+        $data = (float) $datas[1];
+        $this->assertNotNull($data);
+        // Initial value is 20.5, we added a random percent of
+        // this value in [-5%, 5%],
+        // so current value should be in [20.5*0.95, 20.5*1.05]
+        $this->assertGreaterThanOrEqual(19.475, $data);
+        $this->assertLessThanOrEqual(21.525, $data);
+
+        $data = (float) $datas[2];
+        $this->assertNotNull($data);
+        // Initial value is 30.5, we added a random percent of
+        // this value in [-5%, 5%],
+        // so current value should be in [30.5*0.95, 30.5*1.05]
+        $this->assertGreaterThanOrEqual(28.975, $data);
+        $this->assertLessThanOrEqual(32.025, $data);
+
+        $this->assertNull($datas[3]);
     }
 }
