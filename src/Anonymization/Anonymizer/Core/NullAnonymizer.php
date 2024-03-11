deb7e271bcde16cbf97643a966009f1debf97d2b
Simon Mellerin <simon.mellerin@makina-corpus.com>
2024-03-11 11:26:09 +0100

    #114 - Add NullAnonymizer

---
+++ src/Anonymization/Anonymizer/Core/NullAnonymizer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MakinaCorpus\DbToolsBundle\Anonymization\Anonymizer\Core;
+
+use MakinaCorpus\DbToolsBundle\Anonymization\Anonymizer\AbstractAnonymizer;
+use MakinaCorpus\DbToolsBundle\Attribute\AsAnonymizer;
+use MakinaCorpus\QueryBuilder\Query\Update;
+
+#[AsAnonymizer(
+    name: 'null',
+    pack: 'core',
+    description: 'Set to NULL'
+)]
+class NullAnonymizer extends AbstractAnonymizer
+{
+    /**
+     * @inheritdoc
+     */
+    public function anonymize(Update $update): void
+    {
+        $expr = $update->expression();
+        $update->set(
+            $this->columnName,
+            $expr->raw('NULL'),
+        );
+    }
+}
