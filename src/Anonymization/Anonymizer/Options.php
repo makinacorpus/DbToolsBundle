096f2f2893939dceb718fea72ab0a398a63ba580
Simon Mellerin <simon.mellerin@makina-corpus.com>
2024-03-15 12:12:49 +0100

    #97 - Add helpers on Options class to ease type validation

--- src/Anonymization/Anonymizer/Options.php
+++ src/Anonymization/Anonymizer/Options.php
@@ -12,11 +12,15 @@ class Options
 
     public function has(string $name): bool
     {
-        return isset($this->options[$name]);
+        return \array_key_exists($name, $this->options);
     }
 
-    public function get(string $name, mixed $default = null): mixed
+    public function get(string $name, mixed $default = null, bool $required = false): mixed
     {
+        if ($required && (null === $default) && !$this->has($name)) {
+            throw new \InvalidArgumentException(\sprintf("Option '%s' value is required", $name));
+        }
+
         return $this->options[$name] ?? $default;
     }
 
@@ -50,5 +54,68 @@ class Options
             \array_keys($this->options),
             \array_values($this->options),
         ));
+    }
+
+    public function getString(string $name, string $default = null, bool $required = false): ?string
+    {
+        $value = $this->get($name, $default, $required);
+        if ($value === null || is_scalar($value) || $value instanceof \Stringable) {
+            return (string) $value;
+        }
+
+        throw new \InvalidArgumentException(\sprintf("Option '%s' value should be a string", $name));
+    }
+
+    public function getInt(string $name, int $default = null, bool $required = false): ?int
+    {
+        $value = $this->get($name, $default, $required);
+
+        if (\is_numeric($value)) {
+            return (int) $value;
+        }
+
+        throw new \InvalidArgumentException(\sprintf("Option '%s' value should be an int", $name));
+    }
+
+    public function getFloat(string $name, float $default = null, bool $required = false): ?float
+    {
+        $value = $this->get($name, $default, $required);
+
+        if (\is_numeric($value)) {
+            return (float) $value;
+        }
+
+        throw new \InvalidArgumentException(\sprintf("Option '%s' value should be a float", $name));
+
+    }
+
+    public function getDate(string $name, \DateTimeImmutable $default = null, bool $required = false): ?\DateTimeImmutable
+    {
+        $value = $this->get($name, $default, $required);
+
+        if ($value instanceof \DateTimeInterface) {
+            return \DateTimeImmutable::createFromInterface($value);
+        }
+
+        try {
+            return new \DateTimeImmutable($value);
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException(\sprintf("Option '%s' value could not be converted to DateTimeImmutable", $name));
+        }
+    }
+
+    public function getInterval(string $name, \DateInterval $default = null, bool $required = false): ?\DateInterval
+    {
+        $value = $this->get($name, $default, $required);
+
+        if ($value instanceof \DateInterval) {
+            return $value;
+        }
+
+        try {
+            return new \DateInterval($value);
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException(\sprintf("Option '%s' value could not be converted to DateInterval", $name));
+        }
     }
 }
--- tests/Unit/Anonymization/OptionsTest.php
+++ tests/Unit/Anonymization/OptionsTest.php
@@ -17,7 +17,7 @@ class OptionsTest extends TestCase
         ]);
 
         self::assertTrue($options->has('foo'));
-        self::assertFalse($options->has('bar'), "null values are not considered being set");
+        self::assertTrue($options->has('bar'), "null values are considered being set");
         self::assertFalse($options->has('baz'));
     }
 
@@ -58,4 +58,137 @@ class OptionsTest extends TestCase
         self::assertSame("3", $options->get('bar', "3"));
         self::assertSame(7, $options->get('bar', 7));
     }
+
+    public function testGetWithRequired(): void
+    {
+        $options = new Options([
+            'foo' => "1",
+            'bar' => null,
+        ]);
+
+        self::assertSame("1", $options->get('foo', null, true));
+        self::assertSame(null, $options->get('bar', null, true));
+
+        // Trying to get ungiven required option with no default
+        // value should lead to an exception
+        self::expectExceptionMessageMatches("@value is required@");
+        $options->get('baz', null, true);
+
+
+        // Trying to get ungiven required option with default
+        // value should not lead to an exception
+        self::assertSame("2", $options->get('baz', "2", true));
+
+    }
+
+    public function testGetString() {
+        $options = new Options([
+            'ok1' => "test",
+            'ok2' => 1,
+            'ok3' => 12.5,
+            'ok4' => new class() {
+                public function __toString()
+                {
+                    return 'test';
+                }
+            },
+            'ko1' => new \DateTimeImmutable(),
+            'ko2' => ['test'],
+        ]);
+
+        self::assertSame("test", $options->getString('ok1'));
+        self::assertSame("1", $options->getString('ok2'));
+        self::assertSame("12.5", $options->getString('ok3'));
+        self::assertIsString($options->getString('ok4'));
+
+        self::expectExceptionMessageMatches("@value should be a string@");
+        $options->getString('ko1');
+        self::expectExceptionMessageMatches("@value should be a string@");
+        $options->getString('ko2');
+    }
+
+    public function testGetInt() {
+        $options = new Options([
+            'ok1' => "1",
+            'ok2' => 1,
+            'ok3' => 12.5,
+            'ko1' => new \DateTimeImmutable(),
+            'ko2' => ['test'],
+        ]);
+
+        self::assertSame(1, $options->getInt('ok1'));
+        self::assertSame(1, $options->getInt('ok2'));
+        self::assertSame(12, $options->getInt('ok3'));
+
+        self::expectExceptionMessageMatches("@value should be an int@");
+        $options->getInt('ko1');
+        self::expectExceptionMessageMatches("@value should be an int@");
+        $options->getInt('ko2');
+
+    }
+
+    public function testGetFloat() {
+        $options = new Options([
+            'ok1' => "1",
+            'ok2' => 1,
+            'ok3' => 12.5,
+            'ko1' => new \DateTimeImmutable(),
+            'ko2' => ['test'],
+        ]);
+
+        self::assertSame(1.0, $options->getFloat('ok1'));
+        self::assertSame(1.0, $options->getFloat('ok2'));
+        self::assertSame(12.5, $options->getFloat('ok3'));
+
+        self::expectExceptionMessageMatches("@value should be a float@");
+        $options->getFloat('ko1');
+        self::expectExceptionMessageMatches("@value should be a float@");
+        $options->getFloat('ko2');
+    }
+
+    public function testGetDate() {
+        $options = new Options([
+            'ok1' => new \DateTimeImmutable(),
+            'ok2' => new \DateTime(),
+            'ok3' => '2 years ago',
+            'ok4' => '2022-01-24',
+            'ok5' => '2022-01-24 00:25',
+            'ko1' => 'test',
+            'ko2' => ['test'],
+            'ko3' => 123456789,
+        ]);
+
+        self::assertInstanceOf(\DateTimeImmutable::class, $options->getDate('ok1'));
+        self::assertInstanceOf(\DateTimeImmutable::class, $options->getDate('ok2'));
+        self::assertInstanceOf(\DateTimeImmutable::class, $options->getDate('ok3'));
+        self::assertInstanceOf(\DateTimeImmutable::class, $options->getDate('ok4'));
+        self::assertInstanceOf(\DateTimeImmutable::class, $options->getDate('ok5'));
+
+        self::expectExceptionMessageMatches("@could not be converted to DateTimeImmutable@");
+        $options->getDate('ko1');
+        self::expectExceptionMessageMatches("@could not be converted to DateTimeImmutable@");
+        $options->getDate('ko2');
+        self::expectExceptionMessageMatches("@could not be converted to DateTimeImmutable@");
+        $options->getDate('ko3');
+
+    }
+
+    public function testGetInterval() {
+        $options = new Options([
+            'ok1' => 'P1D',
+            'ok2' => 'PT36S',
+            'ko1' => 'test',
+            'ko2' => ['test'],
+        ]);
+
+        self::assertInstanceOf(\DateInterval::class, $options->getInterval('ok1'));
+        self::assertInstanceOf(\DateInterval::class, $options->getInterval('ok2'));
+
+        self::expectExceptionMessageMatches("@could not be converted to DateInterval@");
+        $options->getInterval('ko1');
+        self::expectExceptionMessageMatches("@could not be converted to DateInterval@");
+        $options->getInterval('ko2');
+
+    }
+
 }
