2b2054fc115dc7ba3215eb73d203204160decc31
Pierre <pounard@users.noreply.github.com>
2024-04-19 13:58:47 +0200

    various improvements to ./dev.sh (#143)
    * no issue - allow testing locally with PHPVER=8-3 and LOWEST=1
    
    * no issue - add -x, -l, -p 8.3 to ./dev.sh script
    
    * no issue - various dev.sh improvements

--- dev.sh
+++ dev.sh
@@ -4,33 +4,77 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# PHP version.
+PHPVER="8-2"
+# Extra parameters passed to all docker command lines.
+EXTRA_DOCKER_ENV=""
+
+# Parse arguments.
+while getopts ":xp:l" opt; do
+    case "${opt}" in
+        x)
+            EXTRA_DOCKER_ENV="${EXTRA_DOCKER_ENV} -e XDEBUG_TRIGGER=1"
+            ;;
+        l)
+            LOWEST=1
+            ;;
+        p)
+            p=${OPTARG}
+            case "${OPTARG}" in
+                "8.3")
+                    PHPVER="8-3"
+                    ;;
+                *)
+                    PHPVER="8-2"
+                    ;;
+            esac
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# phpunit container variant to use.
+PHPUNIT_CONTAINER="phpunit-${PHPVER}"
+
 section_title() {
     printf "${RED}\n-------------------------------- ${NC}"
     printf "${RED}$1${NC}"
     printf "${RED} --------------------------------\n${NC}"
 }
 
+# Run docker compose for project
+do_docker_compose() {
+    docker compose -p db_tools_bundle_test "$@"
+}
+
 # Build docker containers
 do_build() {
     section_title "Rebuilding containers"
-    docker compose -p db_tools_bundle_test build;
+    do_docker_compose build;
 }
 
 # Start docker containers
 do_up() {
     section_title "Up containers"
-    docker compose -p db_tools_bundle_test up -d --force-recreate --remove-orphans
+    do_docker_compose up -d --force-recreate --remove-orphans
 }
 
 # Stop docker containers
 do_down() {
     section_title "Down containers"
-    docker compose -p db_tools_bundle_test down
+    do_docker_compose down
 }
 
 do_composer_update() {
     echo 'composer update'
-    docker compose -p db_tools_bundle_test exec phpunit composer update
+    if [[ -z "${LOWEST}" ]]; then
+        do_docker_compose exec $PHPUNIT_CONTAINER composer update
+    else
+        do_docker_compose exec $PHPUNIT_CONTAINER composer update --prefer-lowest
+    fi
 }
 
 # Launch composer checks (for Static analysis & Code style fixer)
@@ -40,18 +84,22 @@ do_checks() {
     do_composer_update
 
     echo 'composer checks'
-    docker compose -p db_tools_bundle_test exec phpunit composer checks
+    do_docker_compose exec $PHPUNIT_CONTAINER composer checks
 }
 
 # Launch PHPUnit tests without any database vendor
 do_unittest() {
     section_title "PHPUnit unit tests"
-    docker compose -p db_tools_bundle_test exec phpunit vendor/bin/phpunit
+    do_docker_compose exec $PHPUNIT_CONTAINER vendor/bin/phpunit
+}
+
+do_ps() {
+    do_docker_compose ps
 }
 
 do_test_mysql57() {
     section_title "Running tests with MySQL 5.7"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_mysql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=mysql57 \
@@ -61,12 +109,12 @@ do_test_mysql57() {
         -e DBAL_ROOT_USER="root" \
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mysql57:3306/test_db?serverVersion=5.7 \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_mysql80() {
     section_title "Running tests with MySQL 8.0"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_mysql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=mysql80 \
@@ -76,12 +124,12 @@ do_test_mysql80() {
         -e DBAL_ROOT_USER=root \
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mysql80:3306/test_db?serverVersion=8 \
-        phpunit vendor/bin/phpunit $@
+        "$PHPUNIT_CONTAINER" vendor/bin/phpunit "$@"
 }
 
 do_test_mysql83() {
     section_title "Running tests with MySQL 8.3"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_mysql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=mysql83 \
@@ -91,12 +139,12 @@ do_test_mysql83() {
         -e DBAL_ROOT_USER=root \
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mysql83:3306/test_db?serverVersion=8 \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_mariadb11() {
     section_title "Running tests with MariaDB 11"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_mysql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=mariadb11 \
@@ -106,21 +154,21 @@ do_test_mariadb11() {
         -e DBAL_ROOT_USER="root" \
         -e DBAL_USER=root \
         -e DATABASE_URL=mysql://root:password@mariadb11:3306/test_db?serverVersion=mariadb-11.1.3 \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_mysql() {
     # @todo Temporary deactivated MySQL 5.7 due to a bug.
     # https://github.com/makinacorpus/DbToolsBundle/issues/124
     # do_test_mysql57
-    do_test_mysql80 $@
-    do_test_mysql83 $@
-    do_test_mariadb11 $@
+    do_test_mysql80 "$@"
+    do_test_mysql83 "$@"
+    do_test_mariadb11 "$@"
 }
 
 do_test_postgresql10() {
     section_title "Running tests with PostgreSQL 10"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_pgsql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=postgresql10 \
@@ -130,12 +178,12 @@ do_test_postgresql10() {
         -e DBAL_ROOT_USER=postgres \
         -e DBAL_USER=postgres \
         -e DATABASE_URL="postgresql://postgres:password@postgresql10:5432/test_db?serverVersion=10&charset=utf8" \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_postgresql16() {
     section_title "Running tests with PostgreSQL 16"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_pgsql \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=postgresql16 \
@@ -145,17 +193,17 @@ do_test_postgresql16() {
         -e DBAL_ROOT_USER=postgres \
         -e DBAL_USER=postgres \
         -e DATABASE_URL="postgresql://postgres:password@postgresql16:5432/test_db?serverVersion=16&charset=utf8" \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_postgresql() {
-    do_test_postgresql10 $@
-    do_test_postgresql16 $@
+    do_test_postgresql10 "$@"
+    do_test_postgresql16 "$@"
 }
 
 do_test_sqlsrv2019() {
     section_title "Running tests with SQL Server 2019"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_sqlsrv \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=sqlsrv2019 \
@@ -165,12 +213,12 @@ do_test_sqlsrv2019() {
         -e DBAL_ROOT_USER=sa \
         -e DBAL_USER=sa \
         -e DATABASE_URL="pdo-sqlsrv://sa:P%40ssword123@sqlsrv2019:1433/test_db?serverVersion=2019&charset=utf8&driverOptions[TrustServerCertificate]=true" \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_sqlsrv2022() {
     section_title "Running tests with SQL Server 2022"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_sqlsrv \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=sqlsrv2022 \
@@ -180,25 +228,25 @@ do_test_sqlsrv2022() {
         -e DBAL_ROOT_USER=sa \
         -e DBAL_USER=sa \
         -e DATABASE_URL="pdo-sqlsrv://sa:P%40ssword123@sqlsrv2022:1433/test_db?serverVersion=2022&charset=utf8&driverOptions[TrustServerCertificate]=true" \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 do_test_sqlsrv() {
-    do_test_sqlsrv2019 $@
-    do_test_sqlsrv2022 $@
+    do_test_sqlsrv2019 "$@"
+    do_test_sqlsrv2022 "$@"
 }
 
 # SQLite version depends upon the PHP embeded version or linked
 # library, we cannot target X or Y version.
 do_test_sqlite() {
     section_title "Running tests with SQLite"
-    docker compose -p db_tools_bundle_test exec \
+    do_docker_compose exec $EXTRA_DOCKER_ENV \
         -e DBAL_DRIVER=pdo_sqlite \
         -e DBAL_DBNAME=test_db \
         -e DBAL_HOST=127.0.0.1 \
         -e DBAL_PATH="test_db.sqlite" \
         -e DATABASE_URL="pdo-sqlite:///test_db.sqlite" \
-        phpunit vendor/bin/phpunit $@
+        $PHPUNIT_CONTAINER vendor/bin/phpunit "$@"
 }
 
 # Run PHPunit tests for all database vendors
@@ -208,14 +256,14 @@ do_test_all() {
     # @todo Temporary deactivated MySQL 5.7 due to a bug.
     # https://github.com/makinacorpus/DbToolsBundle/issues/124
     # do_test_mysql57
-    do_test_mysql80 $@
-    do_test_mysql83 $@
-    do_test_mariadb11 $@
-    do_test_postgresql10 $@
-    do_test_postgresql16 $@
-    do_test_sqlsrv2019 $@
-    do_test_sqlsrv2022 $@
-    do_test_sqlite $@
+    do_test_mysql80 "$@"
+    do_test_mysql83 "$@"
+    do_test_mariadb11 "$@"
+    do_test_postgresql10 "$@"
+    do_test_postgresql16 "$@"
+    do_test_sqlsrv2019 "$@"
+    do_test_sqlsrv2022 "$@"
+    do_test_sqlite "$@"
 }
 
 do_test_notice() {
@@ -279,6 +327,12 @@ do_notice() {
     printf "\n  - ${GREEN}test${NC}: Run PHPUnit tests for a specific database vendors or version"
     printf "\n  - ${GREEN}unittest${NC}: Run PHPUnit tests without any database vendor"
     printf "\n  - ${GREEN}notice${NC}: Display this help"
+    printf "\n"
+    printf "\nAvailable options:"
+    printf "\n  ${GREEN}-l${NC}: run ${GREEN}composer update${NC} with ${GREEN}--prefer-lowest${NC} option"
+    printf "\n  ${GREEN}-x${NC}: trigger ${GREEN}xdebug${NC} when running test suites (ignored otherwise)"
+    printf "\n  ${GREEN}-p 8.3${NC}: choose PHP version to run, ${GREEN}8.2${NC} or ${GREEN}8.3${NC}"
+    printf "\n"
     printf "\n\n"
 }
 
@@ -288,6 +342,6 @@ action=${1-}
 if [[ -n $@ ]];then shift;fi
 
 case $action in
-    build|up|down|checks|test_all|unittest|test|composer_update|notice) do_$action "$@";;
+    build|up|down|ps|checks|test_all|unittest|test|composer_update|notice) do_$action "$@";;
     *) do_notice;;
 esac
