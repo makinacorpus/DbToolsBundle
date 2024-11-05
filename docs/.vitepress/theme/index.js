d4d8577365516794a41ccd22cd129e667939a52e
Simon Mellerin <simon.mellerin@makina-corpus.com>
2024-11-05 11:40:04 +0100

    Docs - add stats on home

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,13 +4,16 @@ import './custom.css'
 import MakinaCorpusHorizontal from './MakinaCorpusHorizontal.vue'
 import MakinaCorpusSquare from './MakinaCorpusSquare.vue'
 import { h } from 'vue'
+import DbToolsBundleActions from './DbToolsBundleActions.vue'
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'aside-bottom': () => h(MakinaCorpusSquare),
       'home-features-after': () => h(MakinaCorpusHorizontal),
+      'home-hero-actions-after': () => h(DbToolsBundleActions)
     })
-  }
+  },
+  VPHomeHero: DbToolsBundleActions,
 }
