65302a0fa143bdd5e26cc75700d78298d73f8ed6
Christopher Georg <christopher.georg@sr-travel.de>
2025-11-01 11:45:29 +0100

    feat: allow phpunit 11 and 12

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
+ErrorHandler::register(null, false);
+
+if (!($loader = @include __DIR__ . '/../vendor/autoload.php')) {
+    echo <<<'EOT'
+You need to install the project dependencies using Composer:
+$ wget https://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT;
+    exit(1);
+}
